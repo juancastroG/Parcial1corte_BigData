3b7b0c0c64159949f311233394afe0cc69c1a653
juancastroG <68920399+juancastroG@users.noreply.github.com>
2023-03-11 16:16:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import requests
 import random
 #beatifoul soup
-from bs4 import BeautifulSoup
 import boto3
 import json
 import datetime
@@ -11,17 +10,24 @@ def peticion():
         'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0',   
     ]
     r = requests.get('https://homes.mitula.com/searchRE/q-chapinero', headers={'User-Agent': random.choice(user_agents_list)})
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = r.text
     return soup
+
     
     
     
 def app(event, context):
     datos = peticion()
-    client = boto3.client('s3')
+
+    s3 = boto3.resource('s3')
     actual = datetime.datetime.now()
     nombre = f"{actual.year}-{actual.strftime('%m')}-{actual.strftime('%d')}.txt"
-    client.put_object(Body=datos, Bucket='daticosparcial', Key=nombre)
+    bucket = s3.Bucket('daticosparcial')
+    print('Se conecto al bucket')
+    print(datos)
+    bucket.put_object(Key='hola.txt', Body=datos)
+    
+    print('PAso')
     return {
         'statusCode': 200,
         'body': json.dumps('Se guardan los datos')
