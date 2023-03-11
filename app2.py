771822886f012960477a41d2c17b353bad1208bb
juancastroG <68920399+juancastroG@users.noreply.github.com>
2023-03-11 17:06:40 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -27,10 +27,8 @@ def poner_datos(event, context):
         diccionario_casas['descripcion'] = casa.split('"description": ')[1].split('",')[0]
         lista_info.append(diccionario_casas)
     #poner la info en un bucket de s3
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket('casasfinalparcial')
-    print('Se conecto al bucket')
-    bucket.put_object(Key='final.txt', Body=str(lista_info))
+    client = boto3.client('s3')
+    client.put_object(Body=str(lista_info), Bucket='casasfinalparcial', Key='final.txt)
 
 
     return {
